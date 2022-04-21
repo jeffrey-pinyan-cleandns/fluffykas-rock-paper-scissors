a519ab293115430102c08473d5d617c2b204eba7
FluffyKas <maruzs.zso@gmail.com>
2022-04-21 16:27:34 +0200

    added Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './assets/global-styles/global-styles.scss';
+import "./assets/global-styles/reset.scss";
+import { Header } from "./components";
 
 function App() {
   return (
     <div className="App">
-      This is an app
+      <Header />
     </div>
   );
 }
