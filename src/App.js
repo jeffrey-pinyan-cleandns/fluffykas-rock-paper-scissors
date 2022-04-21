d7aaf8f47c0a41d5c9edfbd88d7c5996eb828e04
FluffyKas <maruzs.zso@gmail.com>
2022-04-21 16:53:29 +0200

    RulesBtn added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './assets/global-styles/global-styles.scss';
 import "./assets/global-styles/reset.scss";
 import { Header } from "./components";
+import { RulesBtn } from "./components";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <RulesBtn />
     </div>
   );
 }
