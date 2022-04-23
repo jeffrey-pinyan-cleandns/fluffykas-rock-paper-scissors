6b27fea28d342e01acf1ac709bcfe076458b2b71
FluffyKas <maruzs.zso@gmail.com>
2022-04-23 14:43:37 +0200

    Adding function that updates game state

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,13 +1,20 @@
 import './Home.scss';
 import { Link } from "react-router-dom";
+import { useGame } from "../../context";
 
 export const Home = () => {
+  const { setGame } = useGame();
+
+  const selectGame = (e) => {
+    setGame(e.target.textContent);
+  }
+
   return (
     <div className="home">
       <h1 className="home-title">What would you like to play?</h1>
       <div className="game-link-container">
-        <Link to="/game" className="game-link">Rock, Paper, Scissors</Link>
-        <Link to="/game" className="game-link">Rock, Paper, Scissors, Lizard, Spock</Link>
+        <Link to="/game" className="game-link" onClick={selectGame}>Rock, Paper, Scissors</Link>
+        <Link to="/game" className="game-link" onClick={selectGame}>Rock, Paper, Scissors, Lizard, Spock</Link>
       </div>
     </div>
   );
