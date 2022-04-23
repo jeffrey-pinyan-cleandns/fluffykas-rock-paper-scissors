35400e4a0481bc53b05a572be665ed06de33f2a7
FluffyKas <maruzs.zso@gmail.com>
2022-04-23 15:26:10 +0200

    Updated images to change based on game state.

--- src/pages/gamecontainer/GameContainer.js
+++ src/pages/gamecontainer/GameContainer.js
@@ -1,25 +1,28 @@
 import "./GameContainer.scss";
-import image from "../../assets/images/bg-triangle.svg";
 import rock from "../../assets/images/icon-rock.svg";
 import paper from "../../assets/images/icon-paper.svg";
 import scissors from "../../assets/images/icon-scissors.svg";
 import lizard from "../../assets/images/icon-lizard.svg";
 import spock from "../../assets/images/icon-spock.svg";
 import { Header, GameBtn, RulesBtn } from "../../components";
+import { useGame } from "../../context";
 
 export const GameContainer = () => {
 
-  let btnImages = [rock, paper, scissors, lizard, spock]
+  const { game } = useGame();
+
+  const btnImages = (game === "Rock, Paper, Scissors" ? [rock, paper, scissors] : [rock, paper, scissors, lizard, spock])
 
   return (
     <div className="game-container">
       <Header />
-      <img src={image} alt="" />
-      {btnImages.map((image, index) => {
-        return (
-          <GameBtn key={index} btnImage={image} />
-        )
-      })}
+      <div className="game-btn-container">
+        {btnImages.map((image, index) => {
+          return (
+            <GameBtn key={index} btnImage={image} />
+          )
+        })}
+      </div>
       <RulesBtn />
     </div>
   );
