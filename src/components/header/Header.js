8eb399c0b75b7b5c91cc1d22ad7a057ef0031163
FluffyKas <maruzs.zso@gmail.com>
2022-04-23 15:18:14 +0200

    Updated logo and alt text to change based on game state.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,15 @@
 import "./Header.scss";
 import logo from "../../assets/images/logo.svg";
+import logoBonus from "../../assets/images/logo-bonus.svg";
+import { useGame } from "../../context";
 
 export const Header = () => {
+
+  const { game } = useGame();
+
   return (
     <header>
-      <img className="logo" src={logo} alt="Rock Paper Scissors." />
+      <img className="logo" src={game === "Rock, Paper, Scissors" ? logo : logoBonus} alt={game} />
       <div className="score-display">
         <h1 className="score-title">SCORE</h1>
         <span className="score">12</span>
