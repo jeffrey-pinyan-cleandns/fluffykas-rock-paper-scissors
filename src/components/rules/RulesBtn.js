770241c52206cdaf185d261ff1dedd4b97442c60
FluffyKas <maruzs.zso@gmail.com>
2022-04-25 16:48:44 +0200

    Added modal with rules for both mobile and desktop

--- src/components/rules/RulesBtn.js
+++ src/components/rules/RulesBtn.js
@@ -1,7 +1,46 @@
 import "./RulesBtn.scss";
+import rules from "../../assets/images/image-rules.svg";
+import rulesBonus from "../../assets/images/image-rules-bonus.svg";
+import closeBtn from "../../assets/images/icon-close.svg";
+import { useGame } from "../../context";
+import { useState } from "react";
 
 export const RulesBtn = () => {
+
+  const { game } = useGame();
+  const [rulesOpened, setRulesOpened] = useState(false);
+
+  const openRulesModal = () => {
+    setRulesOpened(true);
+  }
+
+  const closeRulesModal = () => {
+    setRulesOpened(false);
+  }
+
   return (
-    <button className="rules-btn">RULES</button>
+    <>
+      <button className="rules-btn" onClick={openRulesModal}>RULES</button>
+      <div className={`rules-modal ${rulesOpened ? "active" : ""}`}>
+        <h2 className="modal-title">Rules</h2>
+        <img
+          src={
+            game === "Rock, Paper, Scissors, Lizard, Spock" ?
+              rulesBonus : rules
+          }
+          alt={
+            game === "Rock, Paper, Scissors, Lizard, Spock" ?
+              "Rules of the game: Rock beats Lizard and Scissors. Lizard beats Paper and Spock. Spock beats. Scissors and Rock. Scissors beats Paper and Lizard. Paper beats Rock and Spock." :
+              "Rules of the game: Rock beats Scissors. Scissors beats Paper. Paper beats Rock."
+          }
+          className="rules-img"
+        />
+        <img
+          src={closeBtn}
+          alt="Close rules."
+          className="close-btn"
+          onClick={closeRulesModal} />
+      </div>
+    </>
   );
 }
