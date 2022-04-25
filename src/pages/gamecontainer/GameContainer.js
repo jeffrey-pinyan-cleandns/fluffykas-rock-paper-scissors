65f229b7f9a53093156275b7d9fd681e5566b6e1
FluffyKas <maruzs.zso@gmail.com>
2022-04-25 12:52:06 +0200

    Adding gameElements to represent buttons

--- src/pages/gamecontainer/GameContainer.js
+++ src/pages/gamecontainer/GameContainer.js
@@ -1,4 +1,6 @@
 import "./GameContainer.scss";
+import bgPentagon from "../../assets/images/bg-pentagon.svg";
+import bgTriangle from "../../assets/images/bg-triangle.svg";
 import rock from "../../assets/images/icon-rock.svg";
 import paper from "../../assets/images/icon-paper.svg";
 import scissors from "../../assets/images/icon-scissors.svg";
@@ -11,15 +13,31 @@ export const GameContainer = () => {
 
   const { game } = useGame();
 
-  const btnImages = (game === "Rock, Paper, Scissors" ? [rock, paper, scissors] : [rock, paper, scissors, lizard, spock])
+  const bgShape = (game === "Rock, Paper, Scissors, Lizard, Spock" ? bgPentagon : bgTriangle);
+
+  const gameElements = (game === "Rock, Paper, Scissors, Lizard, Spock" ?
+    [
+      { name: "rock", image: rock, style: { bottom: 0, right: 0 } },
+      { name: "paper", image: paper, style: { top: "50%", right: 0 } },
+      { name: "scissors", image: scissors, style: { left: "50%" } },
+      { name: "lizard", image: lizard, style: { bottom: 0 } },
+      { name: "spock", image: spock, style: { top: "50%" } }
+    ] :
+    [
+      { name: "rock", image: rock, style: {} },
+      { name: "paper", image: paper, style: { right: 0 } },
+      { name: "scissors", image: scissors, style: { bottom: 0 } }
+    ]
+  );
 
   return (
     <div className="game-container">
       <Header />
       <div className="game-btn-container">
-        {btnImages.map((image, index) => {
+        <img src={bgShape} alt="" />
+        {gameElements.map((element, index) => {
           return (
-            <GameBtn key={index} btnImage={image} />
+            <GameBtn key={index} element={element} />
           )
         })}
       </div>
