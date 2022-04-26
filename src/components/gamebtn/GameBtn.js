3a9b2131ba54ae1bf2981da3971cd4dffba899d4
FluffyKas <maruzs.zso@gmail.com>
2022-04-26 12:55:52 +0200

    Added functionality trigger

--- src/components/gamebtn/GameBtn.js
+++ src/components/gamebtn/GameBtn.js
@@ -1,8 +1,15 @@
 import "./GameBtn.scss";
 
-export const GameBtn = ({ element }) => {
+export const GameBtn = ({ element, setUserChoice, getHouseChoice }) => {
+
+  const triggerUserChoice = () => {
+    setUserChoice(element.name);
+    getHouseChoice();
+  }
+
+
   return (
-    <button className={`${element.name} game-btn`} style={element.style}>
+    <button className={`${element.name} game-btn`} style={element.style} onClick={triggerUserChoice}>
       <img src={element.image} alt={element.name} />
     </button>
   );
