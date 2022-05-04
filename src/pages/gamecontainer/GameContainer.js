25c302b13dd5a9edd71ba900a92f80f10bcc6f7c
FluffyKas <maruzs.zso@gmail.com>
2022-05-04 13:45:10 +0200

    Added ResultContainer, removed temporary result display

--- src/pages/gamecontainer/GameContainer.js
+++ src/pages/gamecontainer/GameContainer.js
@@ -1,32 +1,36 @@
 import "./GameContainer.scss";
 import bgPentagon from "../../assets/images/bg-pentagon.svg";
 import bgTriangle from "../../assets/images/bg-triangle.svg";
-import { Header, GameBtn, RulesBtn } from "../../components";
+import { Header, GameBtn, RulesBtn, ResultContainer } from "../../components";
 import { useGame } from "../../context";
 import dataForGame from "../../data/gameData.json";
 import dataForBonusGame from "../../data/gameDataBonus.json";
 
 export const GameContainer = () => {
 
-  const { game, userChoice, houseChoice, winner } = useGame();
+  const { game, userChoice } = useGame();
   const gameElements = (game === "Rock, Paper, Scissors, Lizard, Spock" ? dataForBonusGame : dataForGame)
 
   return (
     <div className="game-container">
+
       <Header />
-      <div className="game-btn-container">
-        <img src={(game === "Rock, Paper, Scissors, Lizard, Spock" ? bgPentagon : bgTriangle)} alt="" aria-hidden="true" />
-        {gameElements.map((element, index) => {
-          return (
-            <GameBtn key={index} element={element} />
-          )
-        })}
-      </div>
-      {userChoice && <div className="somestuff">{userChoice}, {houseChoice}, {winner}</div>}
-      {userChoice && <div className="results">
-        {/* eeeeh..gameElements.filter... maybe? */}
-      </div>}
+
+      {!userChoice &&
+        <div className="game-btn-container">
+          <img src={(game === "Rock, Paper, Scissors, Lizard, Spock" ? bgPentagon : bgTriangle)} alt="" aria-hidden="true" />
+          {gameElements.map((element, index) => {
+            return (
+              <GameBtn key={index} element={element} />
+            )
+          })}
+        </div>
+      }
+
+      {userChoice && <ResultContainer />}
+
       <RulesBtn />
+
     </div>
   );
 }
