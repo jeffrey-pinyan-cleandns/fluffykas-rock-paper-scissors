f4453ef3e435a0c9bd1259665c220943e7b596ca
FluffyKas <maruzs.zso@gmail.com>
2022-05-04 15:08:38 +0200

    Added page transition

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,8 @@
 import './Home.scss';
 import { Link } from "react-router-dom";
 import { useGame } from "../../context";
+import { pageTransition } from "../../assets/animations";
+import { motion } from "framer-motion";
 
 export const Home = () => {
   const { setGame } = useGame();
@@ -10,12 +12,17 @@ export const Home = () => {
   }
 
   return (
-    <div className="home">
+    <motion.div
+      className="home"
+      variants={pageTransition}
+      initial="hidden"
+      animate="visible"
+    >
       <h1 className="home-title">What would you like to play?</h1>
       <div className="game-link-container">
         <Link to="/game" className="game-link" onClick={selectGame}>Rock, Paper, Scissors</Link>
         <Link to="/game" className="game-link" onClick={selectGame}>Rock, Paper, Scissors, Lizard, Spock</Link>
       </div>
-    </div>
+    </motion.div>
   );
 }
