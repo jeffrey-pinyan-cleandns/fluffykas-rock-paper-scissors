c1be1eb95057307a3b1622ed0902ecc43a982349
FluffyKas <maruzs.zso@gmail.com>
2022-05-04 13:47:29 +0200

    Added imports for new ResultBtn and ResultContainer

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { Header } from "./header/Header";
 export { RulesBtn } from "./rules/RulesBtn";
 export { GameBtn } from "./gamebtn/GameBtn";
+export { ResultBtn } from "./resultbtn/ResultBtn";
+export { ResultContainer } from "./result-container/ResultContainer";
