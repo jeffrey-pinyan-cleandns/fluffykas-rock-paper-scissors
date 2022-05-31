6599258f0dfae27aa81bbb8740f76ba1a485fe54
FluffyKas <maruzs.zso@gmail.com>
2022-05-31 15:05:15 +0200

    fix: update imports

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { Header } from "./header/Header";
-export { RulesBtn } from "./rules/RulesBtn";
+export { RulesModal } from "./rules/RulesModal";
 export { GameBtn } from "./gamebtn/GameBtn";
 export { ResultBtn } from "./resultbtn/ResultBtn";
 export { ResultContainer } from "./result-container/ResultContainer";
