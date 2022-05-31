21bc0dca111ab3d818024f26a8d9ada6672f08d1
FluffyKas <maruzs.zso@gmail.com>
2022-05-31 12:35:08 +0200

    style: adjust button styles
    Introduce separate styles for button placements to look good on both small and large screens. Add state to capture screen width, then wrap it in useEffect so the style changes are applied even when the window is resized. The game button width/height are  also adjusted for small screens.

--- src/components/gamebtn/GameBtn.js
+++ src/components/gamebtn/GameBtn.js
@@ -2,10 +2,26 @@ import "./GameBtn.scss";
 import { useGame } from "../../context";
 import { BtnAnimation } from "../../assets/animations";
 import { motion } from "framer-motion";
+import { useState, useEffect } from "react";
 
 export const GameBtn = ({ element }) => {
 
   const { setUserChoice, getHouseChoice } = useGame();
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  console.log(screenWidth)
+
+  useEffect(() => {
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', changeWidth)
+
+    return () => {
+      window.removeEventListener('resize', changeWidth)
+    }
+
+  }, []);
 
   const triggerUserChoice = () => {
     setUserChoice(element.name);
@@ -14,12 +30,12 @@ export const GameBtn = ({ element }) => {
 
 
   return (
-    <motion.button className={`${element.name} game-btn`} style={element.style} onClick={triggerUserChoice}
+    <motion.button className={`${element.name} game-btn`} style={screenWidth < 960 ? element.style : element.styleDesktop} onClick={triggerUserChoice}
       variants={BtnAnimation}
       initial="hidden"
       animate="visible"
     >
-      <img src={element.image} alt={element.name} />
+      <img src={element.image} alt={element.name} className="game-img" />
     </motion.button>
   );
 }
