950ab295506e3a5704d1cf9577ce247ef8bbf7fd
FluffyKas <maruzs.zso@gmail.com>
2022-05-31 14:11:29 +0200

    animations: add delay animations

--- src/components/result-container/ResultContainer.js
+++ src/components/result-container/ResultContainer.js
@@ -2,6 +2,7 @@ import "./ResultContainer.scss";
 import { useNavigate } from "react-router-dom";
 import { ResultBtn } from "../resultbtn/ResultBtn";
 import { useGame } from "../../context";
+import { motion } from "framer-motion";
 
 export const ResultContainer = () => {
 
@@ -20,19 +21,37 @@ export const ResultContainer = () => {
 
   return (
     <div className="result-container">
-      <div className="choice-container">
+
+      <motion.div
+        className="choice-container"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: .5 }}
+      >
         <h2>You picked</h2>
         <ResultBtn choice={userChoice} />
-      </div>
-      <div className="result-display">
+      </motion.div>
+
+      <motion.div
+        className="result-display"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 5.5 }}
+      >
         <h1>{result}</h1>
         <button onClick={resetUserChoice}>Play again</button>
         <button onClick={resetGame}>Reset game</button>
-      </div>
-      <div className="choice-container">
+      </motion.div>
+
+      <motion.div
+        className="choice-container"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1.75 }}
+      >
         <h2>The house picked</h2>
         <ResultBtn choice={houseChoice} />
-      </div>
+      </motion.div>
     </div>
   );
 }
