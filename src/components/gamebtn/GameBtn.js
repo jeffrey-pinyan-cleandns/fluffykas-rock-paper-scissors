cc8922591d58b7e38bd4d7abce2be8e5d6ed4b15
jeffrey-pinyan-cleandns <jeffrey.pinyan@cleandns.email>
2022-06-01 22:31:32 -0400

    fix: removed eventListener for screen width

--- src/components/gamebtn/GameBtn.js
+++ src/components/gamebtn/GameBtn.js
@@ -2,32 +2,17 @@ import "./GameBtn.scss";
 import { useGame } from "../../context";
 import { BtnAnimation } from "../../assets/animations";
 import { motion } from "framer-motion";
-import { useState, useEffect } from "react";
+import { useCallback } from "react";
 
 export const GameBtn = ({ element }) => {
 
   const { setUserChoice, getHouseChoice } = useGame();
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-  console.log(screenWidth)
+  const screenWidth = window.innerWidth;
 
-  useEffect(() => {
-    const changeWidth = () => {
-      setScreenWidth(window.innerWidth);
-    }
-
-    window.addEventListener('resize', changeWidth)
-
-    return () => {
-      window.removeEventListener('resize', changeWidth)
-    }
-
-  }, []);
-
-  const triggerUserChoice = () => {
+  const triggerUserChoice = useCallback(() => {
     setUserChoice(element.name);
     getHouseChoice();
-  }
-
+  }, [element, setUserChoice, getHouseChoice]);
 
   return (
     <motion.button className={`${element.name} game-btn`} style={screenWidth < 960 ? element.style : element.styleDesktop} onClick={triggerUserChoice}
